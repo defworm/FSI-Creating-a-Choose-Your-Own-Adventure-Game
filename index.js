a436c6a2503a096c3a7feae76120436dcc667f05
Crystal Korth <crystalkorth@gmail.com>
2022-03-30 14:26:52 -0500

    added more equal signs

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 //Your Code here
 response = window.prompt ("Hello")
-if(response = "Hi") {
+if(response === "Hi") {
     answer = window.prompt ("Nice to meet you!")
-    if (answer == 
+    if (answer === "Whatever" 
 } else {
     window.prompt ("I wish you were nicer...")
 }
