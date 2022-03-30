1f3f3924ea7187a96f3420a74d618360a3989f13
Crystal Korth <crystalkorth@gmail.com>
2022-03-30 14:17:16 -0500

    new window prompts

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 //Your Code here
+response = window.prompt ("Hello")
+if(response = "Hi") {
+    answer = window.prompt ("Nice to meet you!")
+    if (answer == 
+} else {
+    window.prompt ("I wish you were nicer...")
+}
